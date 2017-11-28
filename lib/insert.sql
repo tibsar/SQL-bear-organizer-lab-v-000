b7ee1eec0b3a6894ee9c43d815cbe7af03f7cc9f
Sara Tibbetts <sxt4866@rit.edu>
2017-11-28 17:13:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,1 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Barry", 2, "F", "brown", "chill", 0), (2, "Fred", 4, "F", "brown", "chill", 0);
-
-create table bears (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER,
-  gender CHAR(1),
-  color TEXT,
-  temperament TEXT,
-  alive BOOLEAN
-);
